66694a7734aef367e16381b55889c912956b0723
ferriarnus <61201275+ferriarnus@users.noreply.github.com>
2023-02-02 14:37:36 +0100

    made recipe based

---
+++ src/main/java/com/ferriarnus/liquidburner/recipe/RecipeRegistry.java
@@ -0,0 +1,44 @@
+package com.ferriarnus.liquidburner.recipe;
+
+import com.ferriarnus.liquidburner.LiquidBurner;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.Container;;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.checkerframework.checker.units.qual.C;
+
+public class RecipeRegistry {
+
+    static final DeferredRegister<RecipeType<?>> RECIPES = DeferredRegister.create(ForgeRegistries.RECIPE_TYPES, LiquidBurner.MODID);
+    static final DeferredRegister<RecipeSerializer<?>> SERIALIZERS = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, LiquidBurner.MODID);
+
+    public static void register() {
+        RECIPES.register(FMLJavaModLoadingContext.get().getModEventBus());
+        SERIALIZERS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+    public static RegistryObject<RecipeType<LiquidBurning>> LIQUIDBURNING = RECIPES.register("liquidburning", ()-> new RecipeTypeImpl<LiquidBurning>(new ResourceLocation(LiquidBurner.MODID, "liquidburning")) );
+
+    static {
+        SERIALIZERS.register("liquidburning", ()-> LiquidBurning.SERIALIZER);
+    }
+
+    private static class RecipeTypeImpl<T extends Recipe<?>> implements RecipeType<T> {
+
+        private final ResourceLocation rl;
+
+        public RecipeTypeImpl(ResourceLocation rl) {
+            this.rl = rl;
+        }
+
+        @Override
+        public String toString() {
+            return rl.toString();
+        }
+    }
+
+}
